39322077d73e9a7ebdd81e1d38d378725487986c
Noah B. Howe <noahbhowe@gmail.com>
2017-02-21 13:55:41 -0500

    small database change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // Establish PSQL Database Congifuration
 const pgp            = require('pg-promise')();
-const connection      = 'postgres://localhost:5432/ink';
+const connection      = process.env.DATABASE_URL || 'postgres://localhost:5432/ink';
 const database        = pgp(connection);
 
 module.exports        = database;
